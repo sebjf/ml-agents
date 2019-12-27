e7211d0d6dab59d05b9205418b7800416a519bb9
Sebastian Friston <sebastian.friston@gmail.com>
2019-12-27 16:41:51 +0000

    added sorting and mutation of all but fittest

--- genetic/learn.py
+++ genetic/learn.py
@@ -2,28 +2,35 @@
 from individual import Individual
 from env_manager import EnvManager
 from instrumentation import PlotManager
+import tensorflow as tf
 
 maxSteps = 500
 counter = 0
 logdir = "summariesga/run1/"
 
 generation = []
-generation.append(Individual().mutate(0.5))
-generation.append(Individual().mutate(0.5))
-generation.append(Individual().mutate(0.5))
-generation.append(Individual().mutate(0.5))
 
-plots = PlotManager()
-plots.begin_worker()
-
-for g in generation:
-    plots.instrument_individual(g)
+for _ in range(0,4):
+    generation.append(Individual().mutate(0.5))
 
 envmanager = EnvManager("envs/Windows/PoD.exe", 4, maxSteps)
 envmanager.start_workers()
 
-envmanager.test_generation(generation)
-envmanager.wait()
+plots = PlotManager()
+#plots.begin_worker()
+#plots.instrument_individuals(generation)
+
+for gid in range(0,5):
+    envmanager.test_generation(generation)
+    envmanager.wait()
+
+    generation.sort(key=lambda x: x.fitness, reverse=True)
+
+    for g in generation:
+        print("Fitness (" + str(gid) + "): " + str(g.fitness) + " " + "Elapsed: " + str(g.endtime-g.starttime))
+
+    for i in range(1,len(generation)):
+        generation[i].mutate(0.25)
 
 envmanager.close()
 plots.close()
